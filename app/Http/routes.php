70b9f6dbb61db3e5aa30e2a8b612515e13ac91de
Lester Hurtado <lester@applester.co>
2016-04-02 13:58:11 +0800

    add SmartTransport for testing (11)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -85,10 +85,6 @@ Route::post('test', function() {
 });
 
 Route::get('ip', function() {
-    $externalContent = file_get_contents('http://checkip.dyndns.com/');
-    preg_match('/Current IP Address: \[?([:.0-9a-fA-F]+)\]?/', $externalContent, $m);
-    $externalIp = $m[1];
-
     return $_SERVER['SERVER_ADDR'];
 });
 
