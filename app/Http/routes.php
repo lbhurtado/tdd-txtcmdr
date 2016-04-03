2bb05668ea0827f647dccd65962e1498d8a2e726
Lester Hurtado <lester@applester.co>
2016-04-03 13:24:59 +0800

    test Smart Sender in route (4)

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,6 +16,7 @@ use App\Classes\Messaging\SMS\SmartTransport;
 use App\Classes\Messaging\SMS\Message;
 use Carbon\Carbon;
 use App\Classes\Messaging\SMS\Sender;
+use SoapClient;
 
 Route::get('/', function () {
     return view('welcome');
